e34018923789dd3594bd35bff637636cdf89bb45
Paweł Marciniak <pablomchod@gmail.com>
2019-05-22 15:41:37 +0200

    create nav's mechanism

---
+++ script.js
@@ -0,0 +1,8 @@
+//nav's gear
+const burger = document.querySelector('div.burger');
+
+const navGear = () => {
+    burger.classList.toggle('active');
+};
+
+burger.addEventListener('click', navGear);
